43d10c2f89febde13d6c5298e71ee1ad6fa271aa
sdesaih <samarthdesain@gmail.com>
2025-03-26 10:12:42 +0530

    success

--- Lang.js
+++ Lang.js
@@ -4,7 +4,7 @@ const { translate } = require("@vitalets/google-translate-api");
 
 async function translateToFrench() {
   try {
-    const response = await translate("John D. Rockefeller was an American business magnate, philanthropist, and founder of Standard Oil, dominating the oil industry.", { to: "fr" });
+    const response = await translate("John D. Rockefeller was an American business magnate, philanthropist, and founder of Standard Oil, dominating the oil industry, and if you aim for great business success like John D. Rockefeller, focus on innovation, strategic investments, and long-term vision. ", { to: "fr" });
 
     console.log(`Translated text : ${response.text}`);
   }
